01fd1930375a8cf7187509626091fa2b11fd8a2e
Tomohiko Himura <eiel.hal@gmail.com>
2014-07-18 15:55:12 +0900

    Modify path for install ioncube when use RedHat base OS

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,5 +11,11 @@ include_recipe %w{ apt } if platform?("ubuntu", "debian")
 include_recipe %w{ apache2 apache2::mod_php5 apache2::mod_rewrite }
 include_recipe %w{ php php::module_mysql php::module_gd }
 
+if platform?("centos", "redhat", "scientific", "fedora", "amazon", "oracle")
+  package 'php-mbstring' do
+    action :install
+  end
+end
+
 package 'unzip'
 include_recipe %w{ acms::ioncube acms::database acms::install }
